1c648dffa1fe1081049812a72d30921c86bc65cf
lee <lh.man@aol.com>
2018-01-17 11:00:56 -0500

    teal and material footers

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-app>
+    <v-content>
+      <indigo-footer></indigo-footer>
+
+      <teal-footer></teal-footer>
+
+    </v-content>
+  </v-app>
+ </template>
+
+
+
+<script>
+  import IndigoFooter from './IndigoFooter'
+  import TealFooter from './TealFooter'
+
+  export default {
+    components: {
+      IndigoFooter,
+      TealFooter
+    }
+  }
+</script>
